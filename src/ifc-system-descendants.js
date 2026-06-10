8fe9fd4eafff64a0bf15e9e080433f654fae2321
adiaztypsa <adiaz@typsa.es>
2026-06-10 11:45:03 +0200

    UPD-20260610-1145

---
+++ src/ifc-system-descendants.js
@@ -0,0 +1,28 @@
+window.IFC_SYSTEM_DESCENDANTS_REFERENCE = {
+    generatedAt: '20260610:1104',
+    version: 'IFC4.3-derived-static-reference',
+    sources: [
+        'https://ifc43-docs.standards.buildingsmart.org/IFC/RELEASE/IFC4x3/HTML/lexical/IfcSystem.htm',
+        'https://ifc43-docs.standards.buildingsmart.org/IFC/RELEASE/IFC4x3/HTML/lexical/IfcZone.htm',
+        'https://ifc43-docs.standards.buildingsmart.org/IFC/RELEASE/IFC4x3/HTML/lexical/IfcBuildingSystem.htm',
+        'https://ifc43-docs.standards.buildingsmart.org/IFC/RELEASE/IFC4x3/HTML/lexical/IfcBuiltSystem.htm',
+        'https://ifc43-docs.standards.buildingsmart.org/IFC/RELEASE/IFC4x3/HTML/lexical/IfcDistributionSystem.htm',
+        'https://ifc43-docs.standards.buildingsmart.org/IFC/RELEASE/IFC4x3/HTML/lexical/IfcDistributionCircuit.htm',
+        'https://ifc43-docs.standards.buildingsmart.org/IFC/RELEASE/IFC4x3/HTML/lexical/IfcStructuralAnalysisModel.htm',
+        'https://ifc43-docs.standards.buildingsmart.org/IFC/RELEASE/IFC4x3/HTML/lexical/IfcStructuralLoadCase.htm'
+    ],
+    classes: [
+        'IFCSYSTEM',
+        'IFCASSET',
+        'IFCINVENTORY',
+        'IFCSTRUCTURALLOADGROUP',
+        'IFCSTRUCTURALRESULTGROUP',
+        'IFCBUILDINGSYSTEM',
+        'IFCBUILTSYSTEM',
+        'IFCDISTRIBUTIONSYSTEM',
+        'IFCDISTRIBUTIONCIRCUIT',
+        'IFCSTRUCTURALANALYSISMODEL',
+        'IFCZONE',
+        'IFCSTRUCTURALLOADCASE'
+    ]
+};
